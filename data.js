99725109e6a4335075b55b135cdebcdd9dac9826
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 13:42:43 +0900

    data add

--- data.js
+++ data.js
@@ -4,17 +4,49 @@
 const flashcardsData = [
 // fruit
     {
-        questionImage: './images/fruit/apple.jpg',
-        answerText: 'apple',
+        questionImage: './images/fruit/Apple.jpg',
+        answerText: 'Apple',
     },
     {
-        questionImage: './images/fruit/banana.jpg',
-        answerText: 'banana',
+        questionImage: './images/fruit/Banana.jpg',
+        answerText: 'Banana',
+    },
+    {
+        questionImage: './images/fruit/Cherry.jpg',
+        answerText: 'Cherry',
+    },
+    {
+        questionImage: './images/fruit/Grape.jpg',
+        answerText: 'Grape',
+    },
+    {
+        questionImage: './images/fruit/Lemon.jpg',
+        answerText: 'Lemon',
+    },
+    {
+        questionImage: './images/fruit/Orange.jpg',
+        answerText: 'Orange',
+    },
+    {
+        questionImage: './images/fruit/Peach.jpg',
+        answerText: 'Peach',
+    },
+    {
+        questionImage: './images/fruit/Pear.jpg',
+        answerText: 'Pear',
+    },
+    {
+        questionImage: './images/fruit/Pineapple.jpg',
+        answerText: 'Pineapple',
+    },
+    {
+        questionImage: './images/fruit/Strawberry.jpg',
+        answerText: 'Strawberry',
     },
 // animal
     {
-        questionImage: './images/animal/dog.jpg',
-        answerText: 'dog',
+        questionImage: './images/animal/Dog.jpg',
+        answerText: 'Dog',
     },
 
 ];
