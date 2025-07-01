cbe789018f992d9b804821703c6477496d0b5fee
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 13:12:18 +0900

    init

---
+++ data.js
@@ -0,0 +1,14 @@
+// data.js
+// ここにフラッシュカードのデータを記述します。
+
+const flashcardsData = [
+    {
+        questionImage: './images/fruit/apple.jpg', // テスト用の画像URL2
+        answerText: 'apple（りんご）',
+    },
+    {
+        questionImage: './images/fruit/banana.jpg', // テスト用の画像URL3
+        answerText: 'banana（ばなな）',
+    },
+    // 必要に応じて、さらに問題を追加してください。
+];
